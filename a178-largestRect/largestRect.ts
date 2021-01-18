944b1094ec907a3279774a9b6f82098ed276d7d7
Miguel Rios <miguelriosoliveira@gmail.com>
2021-01-18 16:03:57 -0300

    Fixed sub-git

---
+++ a178-largestRect/largestRect.ts
@@ -0,0 +1,22 @@
+/**
+ * Given a n x m binary matrix filled with 0s and 1s,
+ * find the largest rectangle containing only 1s and return its area.
+ *
+ * Example:
+ * $ matrix =
+      [
+        [“1”,”0”,”1”,”0”,”0”],
+        [“1”,”0”,”1”,”1”,”1”],
+        [“1”,”1”,”0”,”1”,”1”],
+        [“1”,”0”,”0”,”1”,”0”]
+      ]
+ * $ largestRect(matrix)
+ * $ 4
+ */
+
+export default function largestRect(matrix: string[][]) {
+  let maxArea = 0;
+  let auxArray = matrix[0];
+
+  return maxArea;
+}
