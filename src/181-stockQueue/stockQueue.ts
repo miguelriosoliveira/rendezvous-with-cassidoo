16d8fb5d3bcc2b129f1feb9be485632482cef325
Miguel Rios <miguelriosoliveira@gmail.com>
2021-02-04 23:29:54 -0300

    Fixed comments indenta

--- src/181-stockQueue/stockQueue.ts
+++ src/181-stockQueue/stockQueue.ts
@@ -1,20 +1,20 @@
-/**
- * You are given a snapshot of a queue of stocks that have changing prices coming in from a stream.
- * Remove the outdated stocks from the queue.
- *
- * Example:
- * $ snapshot = [
-			{ sym: ‘GME’, cost: 280 },
-			{ sym: ‘PYPL’, cost: 234 },
-			{ sym: ‘AMZN’, cost: 3206 },
-			{ sym: ‘AMZN’, cost: 3213 },
-			{ sym: ‘GME’, cost: 325 }
-		]
- * $ stockQueue(snapshot)
- * $ [{ sym: ‘PYPL’, cost: 234 },
-			{ sym: ‘AMZN’, cost: 3213 },
-			{ sym: ‘GME’, cost: 325 }]
- */
+/*
+You are given a snapshot of a queue of stocks that have changing prices coming in from a stream.
+Remove the outdated stocks from the queue.
+
+Example:
+$ snapshot = [
+  { sym: ‘GME’, cost: 280 },
+  { sym: ‘PYPL’, cost: 234 },
+  { sym: ‘AMZN’, cost: 3206 },
+  { sym: ‘AMZN’, cost: 3213 },
+  { sym: ‘GME’, cost: 325 }
+]
+$ stockQueue(snapshot)
+$ [{ sym: ‘PYPL’, cost: 234 },
+   { sym: ‘AMZN’, cost: 3213 },
+   { sym: ‘GME’, cost: 325 }]
+*/
 
 interface Stock {
 	sym: string;
