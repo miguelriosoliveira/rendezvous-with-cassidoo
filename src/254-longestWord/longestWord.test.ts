4c24d408e6f0ec1cf075c8840cfc69b0e2a7f849
Miguel Rios <miguelriosoliveira@gmail.com>
2022-06-30 13:07:32 +0200

    fix: Handling lack of match

--- src/254-longestWord/longestWord.test.ts
+++ src/254-longestWord/longestWord.test.ts
@@ -5,4 +5,9 @@ describe('testing longText', () => {
 		const longest = longestWord('abppplee', ['able', 'ale', 'apple', 'bale', 'kangaroo']);
 		expect(longest).toBe('apple');
 	});
+
+	it('should return null because no word in dict is subsequence', () => {
+		const longest = longestWord('bpppleea', ['able', 'ale', 'apple', 'bale', 'kangaroo']);
+		expect(longest).toBeNull();
+	});
 });
