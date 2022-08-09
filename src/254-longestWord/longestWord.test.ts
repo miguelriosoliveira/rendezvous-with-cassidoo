8de9870b87f31870c95fa0972fea533e138fc5ab
Miguel Rios <miguelriosoliveira@gmail.com>
2022-08-09 21:07:45 +0200

    improvement: Readability on longestWord

--- src/254-longestWord/longestWord.test.ts
+++ src/254-longestWord/longestWord.test.ts
@@ -1,9 +1,24 @@
 import { longestWord } from './longestWord';
 
-describe('testing longText', () => {
+describe('testing longestWord', () => {
 	it('should return apple', () => {
 		const longest = longestWord('abppplee', ['able', 'ale', 'apple', 'bale', 'kangaroo']);
 		expect(longest).toBe('apple');
+	});
+
+	it('should return able', () => {
+		const longest = longestWord('abplee', ['able', 'ale', 'apple', 'bale', 'kangaroo']);
+		expect(longest).toBe('able');
+	});
+
+	it('should return ale', () => {
+		const longest = longestWord('aflee', ['able', 'ale', 'apple', 'bale', 'kangaroo']);
+		expect(longest).toBe('ale');
+	});
+
+	it('should return null because the single P', () => {
+		const longest = longestWord('abplee', ['cable', 'dale', 'apple', 'bale', 'kangaroo']);
+		expect(longest).toBeNull();
 	});
 
 	it('should return null because no word in dict is subsequence', () => {
