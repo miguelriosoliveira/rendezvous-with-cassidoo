1f9737a6696637b659e90c582155a451e338215c
Miguel Rios <miguelriosoliveira@gmail.com>
2022-08-09 22:46:09 +0200

    improvement: Readability on find2020

--- src/176-find2020/find2020.ts
+++ src/176-find2020/find2020.ts
@@ -4,19 +4,17 @@
  * and -1 if it’s not found in the string.
  *
  * Example:
- * $ find2020(‘2220000202220020200’)
+ * $ find2020('2220000202220020200')
  * $ 14
  */
 
-export default function find2020(text: string): number {
-	const letters = text.split('');
-
-	for (let index = 0; index <= letters.length - 4; index += 1) {
+export function find2020(text: string): number {
+	for (let index = 0; text[index]; index++) {
 		if (
-			letters[index] === '2' &&
-			letters[index + 1] === '0' &&
-			letters[index + 2] === '2' &&
-			letters[index + 3] === '0'
+			text[index] === '2' &&
+			text[index + 1] === '0' &&
+			text[index + 2] === '2' &&
+			text[index + 3] === '0'
 		) {
 			return index;
 		}
