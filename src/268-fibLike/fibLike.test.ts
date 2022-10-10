db655424d8d48b4cfe33c31e90173ee27a101269
Miguel Rios <miguelriosoliveira@gmail.com>
2022-10-10 13:58:51 +0200

    tests: Better coverage for fibLike question

--- src/268-fibLike/fibLike.test.ts
+++ src/268-fibLike/fibLike.test.ts
@@ -4,6 +4,7 @@ describe('testing fibLike', () => {
 	it('should return fibonacci-like sequence', () => {
 		expect(fibLike(10, 20, 5)).toStrictEqual([10, 20, 30, 50, 80]);
 		expect(fibLike(3, 7, 5)).toStrictEqual([3, 7, 10, 17, 27]);
+		expect(fibLike(3, 7, 2)).toStrictEqual([3, 7]);
 		expect(fibLike(0, 1, 10)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
 	});
 
@@ -16,6 +17,7 @@ describe('testing isFibLike', () => {
 	it('should return that is a fibonacci-like sequence', () => {
 		expect(isFibLike([10, 20, 30, 50, 80])).toBe(true);
 		expect(isFibLike([3, 7, 10, 17, 27])).toBe(true);
+		expect(isFibLike([1, 2, 3])).toBe(true);
 		expect(isFibLike([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])).toBe(true);
 	});
 
