b6113734462821168a895241ffb6a4adbe5fe546
Miguel Rios <miguelriosoliveira@gmail.com>
2022-11-16 20:19:03 +0100

    question: combineStrings

---
+++ src/274-combineStrings/README.md
@@ -0,0 +1,24 @@
+# combineStrings
+
+Interview question of the [issue #274 of rendezvous with cassidoo](https://buttondown.email/cassidoo/archive/normal-is-nothing-more-than-a-cycle-on-a-washing/).
+
+## The Question
+
+**Given a list of strings `arr`, and a max size `n`, return a new list where the strings (from left to right) are joined together with a space, so that each new string is less than or equal to the max size.**
+
+Examples:
+
+```js
+> combineStrings(["a", "b", "c", "d", "e", "f", "g"], 5)
+> ["a b c", "d e f", "g"]
+
+> combineStrings(["a", "b", "c", "d", "e", "f", "g"], 12)
+> ["a b c d e f", "g"]
+
+> combineStrings(["alpha", "beta", "gamma", "delta", "epsilon"], 20)
+> ["alpha beta gamma", "delta epsilon"]
+```
+
+## Installing & Running
+
+Just `yarn` to install all dependencies and then `yarn test` to run the tests!
---
+++ src/274-combineStrings/combineStrings.test.ts
@@ -0,0 +1,27 @@
+import { combineStrings } from './combineStrings';
+
+describe('testing combineStrings', () => {
+	it('should combine to a max of 5', async () => {
+		const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+		const n = 5;
+		expect(combineStrings(arr, n)).toStrictEqual(['a b c', 'd e f', 'g']);
+	});
+
+	it('should combine to a max of 12', async () => {
+		const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+		const n = 12;
+		expect(combineStrings(arr, n)).toStrictEqual(['a b c d e f', 'g']);
+	});
+
+	it('should combine to a max of 13', async () => {
+		const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+		const n = 13;
+		expect(combineStrings(arr, n)).toStrictEqual(['a b c d e f g']);
+	});
+
+	it('should combine to a max of 20', async () => {
+		const arr = ['alpha', 'beta', 'gamma', 'delta', 'epsilon'];
+		const n = 20;
+		expect(combineStrings(arr, n)).toStrictEqual(['alpha beta gamma', 'delta epsilon']);
+	});
+});
