3dfff2a758d20e8c7eff335bba422854de9af0cb
Miguel Rios <miguelriosoliveira@gmail.com>
2022-12-19 18:06:36 +0100

    question: capitalAfterVowel

---
+++ src/279-capitalAfterVowel/capitalAfterVowel.test.ts
@@ -0,0 +1,37 @@
+import { capitalAfterVowel, capitalAfterVowelWithRegex } from './capitalAfterVowel';
+
+describe('testing capitalAfterVowel', () => {
+	it('"hello world" should evaluate to "heLlo WoRld"', () => {
+		expect(capitalAfterVowel('hello world')).toBe('heLlo WoRld');
+	});
+
+	it('"xaabeuekadii" should evaluate to "xaaBeueKaDii"', () => {
+		expect(capitalAfterVowel('xaabeuekadii')).toBe('xaaBeueKaDii');
+	});
+
+	it('"cinema information passage" should evaluate to "ciNeMa iNfoRmaTioN paSsaGe"', () => {
+		expect(capitalAfterVowel('cinema information passage')).toBe('ciNeMa iNfoRmaTioN paSsaGe');
+	});
+});
+
+describe('testing capitalAfterVowelWithRegex', () => {
+	it('"hello world" should evaluate to "heLlo WoRld"', () => {
+		expect(capitalAfterVowelWithRegex('hello world')).toBe('heLlo WoRld');
+	});
+
+	it('"xaabeuekadii" should evaluate to "xaaBeueKaDii"', () => {
+		expect(capitalAfterVowelWithRegex('xaabeuekadii')).toBe('xaaBeueKaDii');
+	});
+
+	it('"cinema information passage" should evaluate to "ciNeMa iNfoRmaTioN paSsaGe"', () => {
+		expect(capitalAfterVowelWithRegex('cinema information passage')).toBe(
+			'ciNeMa iNfoRmaTioN paSsaGe',
+		);
+	});
+
+	it('"cinema information passage" should evaluate to "ciNeMa iNfoRmaTioN paSsaGe"', () => {
+		expect(capitalAfterVowelWithRegex('cinema information passage')).toBe(
+			'ciNeMa iNfoRmaTioN paSsaGe',
+		);
+	});
+});
