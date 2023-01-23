40ffa9b1faab6fa73cad93439d04dade46f51f96
Miguel Rios <miguelriosoliveira@gmail.com>
2023-01-23 19:44:13 +0100

    question: missingBits

---
+++ src/284-missingBits/missingBits.test.ts
@@ -0,0 +1,33 @@
+import { missingBits } from './missingBits';
+
+describe('testing missingBits', () => {
+	it('should show 1 gap', () => {
+		const missing = missingBits([1, 2, 3, 4, 20, 21, 22, 23]);
+		expect(missing).toBe('[1,2,3,4,...,20,21,22,23]');
+	});
+
+	it('should fill the gap', () => {
+		const missing = missingBits([1, 2, 3, 5, 6]);
+		expect(missing).toBe('[1,2,3,4,5,6]');
+	});
+
+	it('should fill first gap and show 2 next gaps', () => {
+		const missing = missingBits([1, 3, 20, 27]);
+		expect(missing).toBe('[1,2,3,...,20,...,27]');
+	});
+
+	it('should fill first gap, show 2 next gaps and fill last gap', () => {
+		const missing = missingBits([1, 3, 20, 27, 29]);
+		expect(missing).toBe('[1,2,3,...,20,...,27,28,29]');
+	});
+
+	it('should return empty array', () => {
+		const missing = missingBits([]);
+		expect(missing).toBe('[]');
+	});
+
+	it('should return array with one item', () => {
+		const missing = missingBits([2]);
+		expect(missing).toBe('[2]');
+	});
+});
---
+++ src/284-missingBits/missingBits.ts
@@ -0,0 +1,27 @@
+/*
+You are given a list of positive integers which represents some range of integers which has been truncated.
+Find the missing bits, insert ellipses to show that that part has been truncated, and print it.
+If the consecutive values differ by exactly two, then insert the missing value.
+
+Examples:
+
+> missingBits([1,2,3,4,20,21,22,23])
+> "[1,2,3,4,...,20,21,22,23]"
+
+> missingBits([1,2,3,5,6])
+> "[1,2,3,4,5,6]"
+
+> missingBits([1,3,20,27])
+> "[1,2,3,...,20,...,27]"
+*/
+
+export function missingBits(bits: number[]): string {
+	const result = [
+		...bits.slice(0, -1).map((bit, i) => {
+			const diff = bits[i + 1] - bit;
+			return diff === 1 ? bit : diff === 2 ? `${bit},${bit + 1}` : `${bit},...`;
+		}),
+		bits.at(-1),
+	];
+	return `[${result}]`;
+}
