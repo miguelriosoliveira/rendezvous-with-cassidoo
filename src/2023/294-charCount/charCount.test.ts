44a27743a46b6c01d5bd57da73bfab79a59b1278
Miguel Rios <miguelriosoliveira@gmail.com>
2023-04-03 19:46:07 +0200

    tests: Better coverage

--- src/2023/294-charCount/charCount.test.ts
+++ src/2023/294-charCount/charCount.test.ts
@@ -2,8 +2,7 @@ import { charCount, numToWords } from './charCount';
 
 describe('#charCount', () => {
 	it('should return a number greater then zero', () => {
-		const count = charCount();
-		expect(count).not.toBe('zero');
+		expect(charCount()).not.toBe('zero');
 	});
 });
 
