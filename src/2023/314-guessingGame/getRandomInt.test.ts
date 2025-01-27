c77db9b37f4664f0be14c34077e402273202f83a
Miguel Rios <miguelriosoliveira@gmail.com>
2025-01-27 15:26:57 +0100

    fix: Moves options object to second position

--- src/2023/314-guessingGame/getRandomInt.test.ts
+++ src/2023/314-guessingGame/getRandomInt.test.ts
@@ -4,27 +4,15 @@ describe('#getRandomInt', () => {
 	const min = 1;
 	const max = 10;
 
-	it(
-		'should return a number between min and max',
-		() => {
-			expect(getRandomInt(min, max)).greaterThanOrEqual(min).lessThanOrEqual(max);
-		},
-		{ repeats: 100 },
-	);
+	it('should return a number between min and max', { repeats: 100 }, () => {
+		expect(getRandomInt(min, max)).greaterThanOrEqual(min).lessThanOrEqual(max);
+	});
 
-	it(
-		'should make sure to return the lower bound',
-		() => {
-			expect(getRandomInt(min, max)).toBe(min);
-		},
-		{ retry: 100 },
-	);
+	it('should make sure to return the lower bound', { retry: 100 }, () => {
+		expect(getRandomInt(min, max)).toBe(min);
+	});
 
-	it(
-		'should make sure to return the upper bound',
-		() => {
-			expect(getRandomInt(min, max)).toBe(max);
-		},
-		{ retry: 100 },
-	);
+	it('should make sure to return the upper bound', { retry: 100 }, () => {
+		expect(getRandomInt(min, max)).toBe(max);
+	});
 });
