30e932ef0b3d9d036658493573da1684e6996b50
Miguel Rios <miguelriosoliveira@gmail.com>
2025-06-18 16:13:44 +0200

    fix: Removes eslint/compat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,8 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tsEslint from 'typescript-eslint';
-import { includeIgnoreFile } from '@eslint/compat';
-import { fileURLToPath } from 'node:url';
-
-const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default [
-	includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
 	pluginJs.configs.recommended,
 	...tsEslint.configs.recommended,
 	{
